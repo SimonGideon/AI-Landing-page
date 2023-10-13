280777cf889b25bbedf8a49577749ef0dc431120
SimonGideon <simongideon918@gmail.com>
2023-10-13 16:59:20 +0300

    Initial commit

--- v1_landing_pages/scripts/animated.js
+++ v1_landing_pages/scripts/animated.js
@@ -1,4 +1,20 @@
-const textWrapper = document.querySelector(".text-wrapper");
-        textWrapper.addEventListener("animationend", () => {
-            textWrapper.style.borderRight = "none";
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    const sideMenu = document.querySelector('.side-menu');
+    const pageNumbers = document.querySelectorAll('.pages span');
+    const discoverMore = document.querySelector('.translate');
+  
+    setTimeout(() => {
+      sideMenu.style.bottom = '0';
+    }, 1000);
+  
+    for (let i = 0; i < pageNumbers.length; i++) {
+      setTimeout(() => {
+        pageNumbers[i].classList.add('active');
+      }, 1500 + i * 500);
+    }
+  
+    setTimeout(() => {
+      discoverMore.style.opacity = '1';
+    }, 1500 + pageNumbers.length * 500);
+  });
+  
