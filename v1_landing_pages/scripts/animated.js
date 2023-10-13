677ab4ad095fdb86eb516084e6baa89f074bdd61
SimonGideon <simongideon918@gmail.com>
2023-10-13 16:44:03 +0300

    Add animation to image and header

---
+++ v1_landing_pages/scripts/animated.js
@@ -0,0 +1,4 @@
+const textWrapper = document.querySelector(".text-wrapper");
+        textWrapper.addEventListener("animationend", () => {
+            textWrapper.style.borderRight = "none";
+        });
